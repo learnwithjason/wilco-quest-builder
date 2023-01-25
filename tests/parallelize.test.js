8a40c724008efaafe0c18ed08a8a16cd9f0a6c1d
Lior Brauer <liorbrauer@users.noreply.github.com>
2023-01-25 02:05:11 +0200

    feat: prepare for publishing (#1)
    * add a working jest test
    
    * add hints to each step
    
    * fill in all quest metadata
    
    * added assets (cover image + logo)

--- tests/parallelize.test.js
+++ tests/parallelize.test.js
@@ -1,39 +1,32 @@
-import { getItemAndComments } from './components/Item/utils/ItemFetcher';
-
-let getItemsTimestamp;
-let getCommentsTimestamp;
-
-describe('Check that async calls are made in parallel', () => {
-  beforeEach(() => {
-    jest.mock('./agent.js', () => {
-      return {
-        Items: {
-          get: async () => {
-            return new Promise((resolve) => {
-              setTimeout(() => {
-                getItemsTimestamp = Date.now();
-                resolve();
-              }, 1500);
-            });
-          },
-        },
-        Comments: {
-          forItem: async () => {
-            return new Promise((resolve) => {
-              setTimeout(() => {
-                getCommentsTimestamp = Date.now();
-                resolve();
-              }, 1000);
-            });
-          },
-        },
-      };
+jest.mock("./agent.js", () => {
+  async function sleep(timeInMilliseconds) {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve();
+      }, timeInMilliseconds);
     });
-  });
+  }
 
-  it('calls both the item and comments fetchers at the same time', async () => {
-    await getItemAndComments();
-
-    expect(getCommentsTimestamp).toEqual(getItemsTimestamp);
-  });
+  return {
+    Items: {
+      get: async () => {
+        getItemsRequestTimestamp = Date.now();
+        await sleep(2000);
+      }
+    },
+    Comments: {
+      forItem: async () => {
+        getCommentsRequestTimestamp = Date.now();
+        await sleep(2000);
+      }
+    }
+  }
 });
+
+import { getItemAndComments } from "./components/Item/utils/ItemFetcher";
+let getItemsRequestTimestamp, getCommentsRequestTimestamp = null;
+
+test("both requests for Item and Comment are executed at the same time", async () => {
+  await getItemAndComments();
+  expect(getItemsRequestTimestamp).toEqual(getCommentsRequestTimestamp);
+})
